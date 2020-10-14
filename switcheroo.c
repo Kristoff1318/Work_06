d8e0477badcf63973eb21360ae04fe8de13eba7b
Kristoff1318 <kristoff.misquitta@gmail.com>
2020-10-14 00:26:37 -0400

    Add file

---
+++ switcheroo.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int arr[10];
+    srand(time(NULL));
+    int i;
+
+    for(i = 0; i < 10; i++) arr[i] = rand();
+
+    arr[9] = 0;
+
+    printf("First array:\n");
+    for(i = 0; i < 10; i++) printf("%d\n", arr[i]);
+
+    int sec_arr[10];
+
+    for(i = 0; i < 10; i++) {
+        *(sec_arr + i) = *(9 - i + arr);
+    }
+
+    printf("\nSecond array:\n");
+    for(i = 0; i < 10; i++) printf("%d\n", sec_arr[i]);
+
+}
